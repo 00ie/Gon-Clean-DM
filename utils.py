13f8c30e3658bd931ed3fc332e56923c18d3c6ff
fakersl <84857762+fakersl@users.noreply.github.com>
2025-11-17 20:05:58 -0300

    feat: atualiza configs
    organizando configs em seções lógicas (paths, tema, API, segurança)

--- utils.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def discord_timestamp_from_id(snowflake_id: str) -> str:
-    try:
-        timestamp_ms = (int(snowflake_id) >> 22) + 1420070400000
-        return datetime.utcfromtimestamp(timestamp_ms / 1000).strftime('%Y-%m-%d %H:%M:%S')
-    except Exception:
-        return "Desconhecido"
